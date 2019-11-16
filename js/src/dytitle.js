5ce2af1617ae52b39c05fdca66ba6f2192845dd7
xiongmingjie <1284883526@qq.com>
2019-11-16 11:48:37 +0800

    Site updated: 2019-11-16 11:48:34

--- js/src/dytitle.js
+++ js/src/dytitle.js
@@ -1,3 +1,3 @@
-// build time:Tue Nov 12 2019 11:59:40 GMT+0800 (GMT+08:00)
+// build time:Sat Nov 16 2019 11:48:25 GMT+0800 (GMT+08:00)
 var OriginTitile=document.title;var titleTime;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="shortcut icon"]').attr("href","/TEP.png");document.title="w(ﾟДﾟ)w 出BUG啦！！！！";clearTimeout(titleTime)}else{$('[rel="shortcut icon"]').attr("href","/favicon.png");document.title="♪(^∇^*)又好了。。。 "+OriginTitile;titleTime=setTimeout(function(){document.title=OriginTitile},2e3)}});
 //rebuild by neat 
